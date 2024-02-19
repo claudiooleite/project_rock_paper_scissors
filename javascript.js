75e12cdf1d3829d1381acf12efc8deb5c57bd367
claudiooleite <claudio.leite@hotmail.com>
2024-02-19 11:35:29 +0100

    s

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
 // Rock Paper Scissors\
 
+/*
+function playerSelection(option){
+  if (option === 'Rock'){ 
+    return 'Rock';
+    
+  } else if (option ==='Paper'){ 
+    return 'Paper';
+    
+  } else if (option ==='Scissors'){ 
+    return 'Scissors';
+  } else 
+    return "Invalid option";
+}
+const buttonPaper = document.querySelector('#paper')
+const buttonRock = document.querySelector('#rock')
+const buttonScissors = document.querySelector('#scissors')
+
+const playerChose = playerSelection('Rock');
+console.log(playerChose)
+}
+*/
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
+
+
 
 function computerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -35,6 +62,8 @@ function playRound(playerSelection, computerSelection) {
         return 'Invalid! Repeat';
     }
 }
+
+
 
 let playerSelection = 'rock';
 console.log(playerSelection);
